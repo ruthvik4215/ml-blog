7484976ff01fb1e05c8f7f102fda4a8ddd2d2c42
Ruthvik Bemidi <ruthvik4215@gmail.com>
2021-03-27 20:23:56 +0530

    created new files in index folder for concepts supervised learning and unsupervised laerning.

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+var modal = document.getElementById("mySidenav");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openNav");
+
+window.onclick = function(event) {
+  if (event.target == document.getElementById("mySidenav")) {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+}
+
